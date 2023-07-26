f623af68af64cf35ba777281dcc20189643203dd
Sineray Dayday <sineraytd.code.edu@az>
2023-07-26 11:46:43 +0400

    Add project files.

---
+++ APIIntro/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using APIIntro.Entities; 
+
+namespace APIIntro.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        static List<Category> categories = new List<Category>
+        {
+            new Category { Name = "Ipad" },
+            new Category { Name = "Earpods" },
+            new Category { Name = "Iphone" },
+            new Category { Name = "Accessories" }
+        };
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(categories[0]);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            categories.Add(category);
+            return Ok();
+        }
+    }
+}
